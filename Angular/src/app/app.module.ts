19e00cb9344b6a1764074a9df8ac2caae15ebb0d
Daniel Vosecky <dvosecky@protonmail.com>
2018-10-18 16:45:26 -0500

    set up basic routing, created empty welcome component

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { appRoutes } from './routes';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+// NEED TO VALIDATE INPUT
+
+export class LoginComponent implements OnInit {
+  email :string;
+  password :string;
+  
+  constructor(private router :Router, private route: ActivatedRoute) { }
+
+  onSubmit() {
+    // request code
+    // alert("email: " + this.email + ", password: " + this.password);
+    this.router.navigate(['../welcome'], { relativeTo: this.route });
+  }
 
   ngOnInit() {
   }
