b78f8585e541addf1cd32318969d1258a575ed87
Landon Miller <landon.r.miller@outlook.com>
2018-10-18 16:59:39 -0500

    Invitation created by someone and META is gone(?)

--- Union/src/main/java/com/revature/beans/Invitation.java
+++ Union/src/main/java/com/revature/beans/Invitation.java
@@ -1,23 +1,55 @@
 package com.revature.beans;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="invitation")
 public class Invitation {
-	
-	/*
-	 *     acc_id NUMBER(10),
-    ev_id NUMBER(10),
-    CONSTRAINT invitation_acc_fk FOREIGN KEY (acc_id)
-    REFERENCES account (acc_id),
-    CONSTRAINT invitation_ev_fk FOREIGN KEY (ev_id)
-    REFERENCES account (ev_id)
-	 */
-	/*
-	private Account acc_id;
-	private Account ev_id;
 
-	*/
+	@ManyToOne
+	@JoinColumn( name="acc_id" )
+	private Account acc_id;
+	
+	@ManyToOne
+	@JoinColumn( name="ev_id" )
+	private Event ev_id;
+
+	
+	
+	public Invitation(Account acc_id, Event ev_id) {
+		super();
+		this.acc_id = acc_id;
+		this.ev_id = ev_id;
+	}
+
+	public Invitation() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Invitation [acc_id=" + acc_id + ", ev_id=" + ev_id + "]";
+	}
+
+	public Account getAcc_id() {
+		return acc_id;
+	}
+
+	public void setAcc_id(Account acc_id) {
+		this.acc_id = acc_id;
+	}
+
+	public Event getEv_id() {
+		return ev_id;
+	}
+
+	public void setEv_id(Event ev_id) {
+		this.ev_id = ev_id;
+	}
+
+	
 }
