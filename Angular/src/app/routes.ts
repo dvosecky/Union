2b000d049648f1d50fa7573fbfff7c68e7f8af4d
Daniel Vosecky <dvosecky@protonmail.com>
2018-10-20 22:08:10 -0500

    good-looking login and employee welcome pages added into angular

--- Angular/src/app/routes.ts
+++ Angular/src/app/routes.ts
@@ -1,10 +1,10 @@
-import { WelcomeComponent } from './welcome/welcome.component';
+import { EmpWelcomeComponent } from './emp-welcome/emp-welcome.component';
 import { LoginComponent } from './login/login.component';
 import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'welcome', component: WelcomeComponent},
+    { path: 'emp-welcome', component: EmpWelcomeComponent},
     { path: '',
       redirectTo: 'login',
       pathMatch: 'full' // what is this for?
