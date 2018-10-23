14fa97a1d1af9441c15c8d4c6820813c633c4128
Rene Flores <rene.flores@nevada.unr.edu>
2018-10-23 13:26:22 -0700

    select, delete, insert dao for departm and account

---
+++ Union/src/main/java/com/revature/dao/DepartmentDaoImpl.java
@@ -0,0 +1,68 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.beans.Department;
+import com.revature.util.HibernateUtil;
+
+public class DepartmentDaoImpl {
+
+	public Integer insertDepartment( Department department) {
+		Integer id=null;
+		Session session = HibernateUtil.getSession();
+		Transaction tx= null;
+		
+		try {
+			tx=session.beginTransaction();
+			id=(Integer)session.save(department);
+			tx.commit();
+		}catch( HibernateException e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		
+		return id;
+		
+	}
+
+
+	public void deleteDepartment(Integer id) {
+		Session session = HibernateUtil.getSession();
+		Transaction tx=null;
+		
+		try {
+			tx=session.beginTransaction();
+			session.delete(session.get(Department.class, id));
+			tx.commit();
+		}catch(HibernateException e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		
+		
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List<Department> selectAllDepartment(){
+		List<Department> departments=null;
+		Session session = HibernateUtil.getSession();
+		
+		try {
+		departments = session.createQuery("FROM Department").list();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		
+		return departments;
+		
+	}
+}
