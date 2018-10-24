1269d8a50640c0defd08863f0cf492ab72e830c7
Daniel-McFarlin <danielmcfarlin7@gmail.com>
2018-10-24 14:50:52 -0500

    Added Event Details

--- Angular/src/app/routes.ts
+++ Angular/src/app/routes.ts
@@ -2,6 +2,7 @@ import { AdminWelcomeComponent } from './admin-welcome/admin-welcome.component';
 import { EmpWelcomeComponent } from './emp-welcome/emp-welcome.component';
 import { LoginComponent } from './login/login.component';
 import {ViewEventsComponent} from './view-events/view-events.component';
+import {EventDetailsComponent} from './event-details/event-details.component';
 import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
@@ -9,6 +10,7 @@ export const appRoutes: Routes = [
     { path: 'emp-welcome', component: EmpWelcomeComponent},
     { path: 'admin-welcome', component: AdminWelcomeComponent},
     {path: 'view-events', component: ViewEventsComponent},
+    {path: 'event-details', component: EventDetailsComponent},
     { path: '',
       redirectTo: 'login',
       pathMatch: 'full' // what is this for?
