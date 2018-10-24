91b3e013a5628404eb5c33803351541ee345476b
Daniel Vosecky <dvosecky@protonmail.com>
2018-10-23 21:07:49 -0500

    navbar-fade-and-route-edits

--- Angular/src/app/admin-welcome/admin-welcome.component.ts
+++ Angular/src/app/admin-welcome/admin-welcome.component.ts
@@ -11,10 +11,33 @@ export class AdminWelcomeComponent implements OnInit {
   constructor(private router :Router, private route :ActivatedRoute) { }
 
   logout() {
+    window.scrollTo(0,0);
     this.router.navigate(['../login'], { relativeTo: this.route });
   }
 
   ngOnInit() {
+    this.invisibleNavBar();
+
+    window.addEventListener("scroll", this.updateNavBar);
   }
+
+  invisibleNavBar() {
+    var nav = document.querySelector(".navbar")
+    nav.setAttribute('style', 'visibility: hidden');
+    nav.classList.add('navbar-ontop');
+  }
+	
+	updateNavBar() {
+
+    var nav = document.querySelector(".navbar");
+    var className = "navbar-ontop";
+
+		if (window.scrollY > 30) {
+      nav.classList.remove(className)
+      nav.setAttribute('style', 'visibility: visible');
+    } else {
+      nav.classList.add(className) 
+    }
+	}
 
 }
