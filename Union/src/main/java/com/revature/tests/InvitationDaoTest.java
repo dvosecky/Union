8238a46db7991f334d31e71addba9124d24488d9
Rene Flores <rene.flores@nevada.unr.edu>
2018-10-24 15:45:33 -0700

    removed junit

--- Union/src/main/java/com/revature/tests/InvitationDaoTest.java
+++ Union/src/main/java/com/revature/tests/InvitationDaoTest.java
@@ -1,20 +1,5 @@
 package com.revature.tests;
 
-import org.apache.log4j.Logger;
-import org.junit.AfterClass;
-//import org.junit.BeforeClass;
-import org.junit.Test;
-
 public class InvitationDaoTest {
-	private static final Logger log = Logger.getLogger(Logger.class); 
-	
-//	@BeforeClass
-//	public void 
-	
-	@AfterClass
-	
-	@Test
-	public void selectAllTest() {
-		
-	}
+
 }
