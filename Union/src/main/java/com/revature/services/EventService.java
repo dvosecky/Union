62d98e1ad8e3c9c114a3dcd68f242d1883e2ac88
Rene Flores <rene.flores@nevada.unr.edu>
2018-10-25 19:03:58 -0700

    Add Account/Get All Accounts/Get All Events/Remove Account Servlet

---
+++ Union/src/main/java/com/revature/services/EventService.java
@@ -0,0 +1,17 @@
+package com.revature.services;
+
+import java.util.List;
+
+import com.revature.beans.Event;
+import com.revature.dao.EventDaoImpl;
+
+public class EventService {
+
+	public static List<Event> retrieveAllEvents(){
+		List<Event> events=null;
+		EventDaoImpl manager= new EventDaoImpl();
+		events = manager.getAllEvents();
+		
+		return events;
+	}
+}
---
+++ Union/src/main/java/com/revature/servlet/GetAllAccounts.java
@@ -0,0 +1,38 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.beans.Account;
+import com.revature.services.UserService;
+
+/**
+ * Servlet implementation class GetAllAccounts
+ */
+public class GetAllAccounts extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		List<Account> accounts = null;
+		ObjectMapper om = new ObjectMapper();
+		PrintWriter out = response.getWriter();
+		accounts = UserService.retrieveAllAccount();
+		out.print(om.writeValueAsString(accounts));
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
