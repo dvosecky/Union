f14395a314fbdf6d26fab36f0ee42dcb05b33e32
Daniel-McFarlin <danielmcfarlin7@gmail.com>
2018-10-25 21:37:08 -0500

    some buttons linked, css fixed, nav bar and footer black

--- Angular/src/app/event-details/event-details.component.ts
+++ Angular/src/app/event-details/event-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Session } from '../session';
 
 @Component({
   selector: 'app-event-details',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-details.component.css']
 })
 export class EventDetailsComponent implements OnInit {
+  email :string;
+  password :string;
 
-  constructor() { }
+  constructor(private router :Router, private route: ActivatedRoute,
+              private session :Session) { }
 
-  ngOnInit() {
+  private validateEmail() {
+    if (this.email === undefined || this.email === "") {
+      return false;
+    }
+    return true;
   }
 
-}
+  private validatePassword() {
+    if (this.password == undefined || this.password === "") {
+      return false;
+    }
+    return true;
+  }
+
+  onSubmit() {
+    if (!this.validateEmail() || !this.validatePassword()) {
+      alert('Invalid email or password');
+      return;
+    }
+
+    // send httpRequest to get user information, including user type, which
+    // we need to know to determine which welcome page to go to"
+
+    if (this.email === "admin") {
+      this.session.role = 'admin';
+      this.router.navigate(['../admin-welcome'], { relativeTo: this.route });
+    } else {
+      this.session.role = 'emp';
+      this.router.navigate(['../emp-welcome'], { relativeTo: this.route });
+    }
+  }
+
+  ngOnInit() {
+    window.scrollTo(0, 0);
+  }
+
+  }
