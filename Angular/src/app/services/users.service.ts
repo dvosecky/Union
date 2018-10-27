e040e28b2d25fae0c1633d5096a6b3d6d5f63336
Daniel Vosecky <dvosecky@protonmail.com>
2018-10-27 16:19:00 -0500

    waiting for servlet fix

--- Angular/src/app/services/users.service.ts
+++ Angular/src/app/services/users.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -9,6 +9,9 @@ export class UsersService {
   constructor(private http :HttpClient) { }
 
   getUsers() {
-    this.http.post()
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded'});
+
+    return this.http.post("http://localhost:8085/Union/GetAllAccounts", "", {headers});
   }
 }
--- Angular/src/app/users/users.component.ts
+++ Angular/src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Session } from './../session';
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-users',
@@ -8,11 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor(private session :Session) { }
+  constructor(private session :Session, private service :UsersService) { }
+
+  users;
 
   ngOnInit() {
 
     window.scrollTo(0, 0);
+    this.service.getUsers().subscribe(
+      (data) => {
+        this.users = data;
+        console.log(data);
+      }, (error) => {
+        console.log(error);
+      }
+    )
 
   }
 
