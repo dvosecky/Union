6f018fc5ff8e1e4746286e4c4ec8ad34e1ce023f
Landon Miller <landon.r.miller@outlook.com>
2018-10-27 18:27:10 -0500

    Do small test for DepartmentDaoImpl fix

--- Union/src/main/java/com/revature/tests/DepartmentTest.java
+++ Union/src/main/java/com/revature/tests/DepartmentTest.java
@@ -1,5 +1,7 @@
 package com.revature.tests;
 
+import java.util.List;
+
 import com.revature.beans.Department;
 import com.revature.dao.DepartmentDaoImpl;
 
@@ -8,11 +10,16 @@ public class DepartmentTest {
 	public static void main(String[] args) {
 		DepartmentDaoImpl dd = new DepartmentDaoImpl();
 		Department dep = dd.selectDepartmentById(1);
+		List<Department> deps = null;
 		
+		System.out.println("SELECT ALL");
+		deps = dd.selectAllDepartment();
+		for (Department d : deps) {
+			System.out.println(d);
+		}
 		
-		
-		System.out.println("DELETE");
-		dd.deleteDepartment(1);
+		//System.out.println("DELETE");
+		//dd.deleteDepartment(1);
 		
 		System.exit(0);
 	}
