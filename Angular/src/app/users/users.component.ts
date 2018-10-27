80d95f8bcb04e4663fdea79a3643f3886a153b3a
Daniel Vosecky <dvosecky@protonmail.com>
2018-10-27 15:41:42 -0500

    started service

--- Angular/src/app/users/users.component.ts
+++ Angular/src/app/users/users.component.ts
@@ -10,17 +10,12 @@ export class UsersComponent implements OnInit {
 
   constructor(private session :Session) { }
 
-  admin :boolean = false;
-  emp :boolean = false;
-
   ngOnInit() {
 
     window.scrollTo(0, 0);
-    if (this.session.role === 'admin') {
-      this.admin = true;
-    } else if (this.session.role === 'emp') {
-      this.emp = true;
-    }
+
   }
+
+
 
 }
