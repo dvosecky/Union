df1119990f3fcbccdb17cb8356d99ce759b12f37
Daniel Vosecky <dvosecky@protonmail.com>
2018-10-28 18:39:36 -0500

    everything. creates events and probably invitations.

--- Angular/src/app/services/create-event.service.ts
+++ Angular/src/app/services/create-event.service.ts
@@ -8,14 +8,22 @@ import { Injectable } from '@angular/core';
 export class CreateEventService {
 
   constructor(private http :HttpClient, private session :Session) { }
+  headers :HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/x-www-form-urlencoded'
+  });
 
   createEvent(name, location, time, date, description) {
-    let headers :HttpHeaders = new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded'
-    });
+    let headers = this.headers;
     return this.http.post('http://18.220.118.195:8085/Union/AddEvent',
     'eventName=' + name + '&location=' + location + '&time=' + time +
         '&date=' + date + '&description=' + description + '&accountID=' + this.session.id, {headers});
+  }
+
+  createInvitation(inviteeId, eventId) {
+    let headers = this.headers;
+    return this.http.post('http://18.220.118.195:8085/Union/CreateInvitation',
+      'acc_id=' + this.session.id + '&inv_acc_id=' + inviteeId + '&inv_ev_id=' +
+        eventId + '&inv_priv_flag=0');
   }
 
   editEvent(name, location, time, date, description) {
