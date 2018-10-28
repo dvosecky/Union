100c8b89acd9f242c011bbf79a2c852fb9c0b8fd
Landon Miller <landon.r.miller@outlook.com>
2018-10-28 11:49:39 -0500

    Implement GetAllInvitations for a user

--- Union/src/main/java/com/revature/servlet/GetAllInvitations.java
+++ Union/src/main/java/com/revature/servlet/GetAllInvitations.java
@@ -1,6 +1,8 @@
 package com.revature.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +11,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.dto.InvitationDTO;
+import com.revature.services.InvitationServices;
+
 public class GetAllInvitations extends HttpServlet {
 	private static final Logger log = Logger.getLogger(GetAllInvitations.class);
 	private static final long serialVersionUID = -5751351096717959963L;
 
+	/*	RETURNS EITHER
+	 * JSON string representation of InvitationDTO list
+	 * 
+	 * 401 error code when account id not found in parameters
+	 */
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		log.info("Received request to get all invitations sent to a user. (Not including deny)");
 		
@@ -25,7 +36,16 @@ public class GetAllInvitations extends HttpServlet {
 		}
 		int acc_id = Integer.getInteger(proxyString);
 		
-		//TODO
-		//List<InvitationDTO> = InvitationServices.getAllInvitations(acc_id);
+		//Accessing service layer to get invitation DTO lists
+		List<InvitationDTO> result = InvitationServices.getAllInvitations(acc_id);
+		
+		//Initialization of object mapper and writer for responses now that results are back
+		ObjectMapper om = new ObjectMapper();
+		res.setContentType("application/json");
+		PrintWriter out = res.getWriter();
+		
+		//Returning result
+		log.debug("Using object mapper to write invitation dto list as a JSON string, then writing it in response.");
+		out.print(om.writeValueAsString(result));
 	}
 }
