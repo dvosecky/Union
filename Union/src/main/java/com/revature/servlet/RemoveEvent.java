4a0a433b53c50b60a869492f43059e9144611075
Landon Miller <landon.r.miller@outlook.com>
2018-10-27 20:19:25 -0500

    Servlet comments and logging
    Servlet comments and logging for the following classes:
    
    • RemoveAccount
    
    • RemoveDepartment (some fixes in addition)
    
    • RemoveEvent

--- Union/src/main/java/com/revature/servlet/RemoveEvent.java
+++ Union/src/main/java/com/revature/servlet/RemoveEvent.java
@@ -6,26 +6,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 import com.revature.services.EventService;
 
 
 public class RemoveEvent extends HttpServlet {
+	private static final Logger log = Logger.getLogger(RemoveEvent.class);
 	private static final long serialVersionUID = 1L;
-       
+    
+	/*	RETURNS EITHER
+	 * 200 success code in case of successful removal
+	 * 400 error code in case of failure to remove
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		log.info("Received a request to remove an event.");
+		
+		//Getting info from parameters and doing integer conversion
 		int eventID = Integer.parseInt( request.getParameter("eventid") );
 		
+		//Accessing service layer to remove event
 		if( EventService.removeEvent(eventID)) {
 			response.sendError(200);
 		}
 		
+		log.warn("Sending back an error: event removal unsuccessful.");
 		response.sendError(400);
 		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		log.info("Received a POST request to remove an event.");
 		doGet(request, response);
 	}
 
