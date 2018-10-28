937e826c16ff88b8dacf35af3c2e8634ccdff2f4
Landon Miller <landon.r.miller@outlook.com>
2018-10-27 19:50:14 -0500

    Servlet comments and logging
    Add servlet comments and logging for following classes:
    
    • CreateInvitation
    
    • DeclineInvitation
    
    • GetAllAccounts
    
    • GetEventByAccount
    
    • GetEventByLead

--- Union/src/main/java/com/revature/servlet/CreateInvitation.java
+++ Union/src/main/java/com/revature/servlet/CreateInvitation.java
@@ -7,44 +7,64 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 import com.revature.services.InvitationServices;
 
 public class CreateInvitation extends HttpServlet{
+	private static final Logger log = Logger.getLogger(CreateInvitation.class);
 	private static final long serialVersionUID = -567945821593226875L;
 
+	/*	RETURNS EITHER
+	 * 200 success code in case of successful invite
+	 * 401 error code in case of lacking account id
+	 * 400 error code in case of lacking input
+	 * 500 error code in all other cases
+	 */
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		log.info("Received a request to send a single invite.");
+		
+		//Getting info from parameters and making integer conversions
 		String proxyString = req.getParameter("acc_id");
 		if (proxyString == null) {
+			log.warn("Sending back an error: account id of account sending invite missing from parameters.");
 			res.sendError(401);
 			return;
 		}
 		Integer acc_id = Integer.getInteger(proxyString);
 		proxyString = req.getParameter("inv_acc_id");
 		if (proxyString == null) {
+			log.warn("Sending back an error: account id of account receiving invite missing from parameters.");
 			res.sendError(400);
 			return;
 		}
 		Integer inv_acc_id = Integer.getInteger(proxyString);
 		proxyString = req.getParameter("inv_ev_id");
 		if (proxyString == null) {
+			log.warn("Sending back an error: event id missing from parameters.");
 			res.sendError(400);
 			return;
 		}
 		Integer inv_ev_id = Integer.getInteger(proxyString);
 		proxyString = req.getParameter("inv_priv_flag");
 		if (proxyString == null) {
+			log.warn("Sending back an error: privilege flag missing from parameters.");
 			res.sendError(400);
 			return;
 		}
 		Integer inv_priv_flag = Integer.getInteger(proxyString);
 		
+		//Accessing service layer to send invite
 		boolean result = InvitationServices.invite(acc_id, inv_acc_id, inv_ev_id, inv_priv_flag);
 		
+		//Checking boolean result of invite creation and sending appropriate http code
+		log.trace("Checking boolean result of invite operation.");
 		if (result) {
 			res.sendError(200);
 			return;
 		}
 		
+		log.warn("Sending back an error: invite was unsuccessful.");
 		res.sendError(500);
 	}
 }
--- Union/src/main/java/com/revature/servlet/GetAllAccounts.java
+++ Union/src/main/java/com/revature/servlet/GetAllAccounts.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.dto.AccountDTO;
 import com.revature.services.UserService;
@@ -17,21 +19,30 @@ import com.revature.services.UserService;
  * Servlet implementation class GetAllAccounts
  */
 public class GetAllAccounts extends HttpServlet {
+	private static final Logger log = Logger.getLogger(GetAllAccounts.class);
 	private static final long serialVersionUID = 1L;
        
+	/*	Always returns a JSON representation of a list of all accounts
+	 * Specifically, a list of AccountDTOs: (List<AccountDTO>)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		log.info("Received a request to get all accounts.");
+		
+		//Initialization
 		List<AccountDTO> accounts = null;
 		ObjectMapper om = new ObjectMapper();
 		PrintWriter out = response.getWriter();
-		accounts = UserService.retrieveAllAccount();
-		out.print(om.writeValueAsString(accounts));
 		
+		//Accessing service layer to retreive all accounts
+		accounts = UserService.retrieveAllAccount();
+		
+		//Using object mapper to return a JSON representation of account list
+		out.print(om.writeValueAsString(accounts));
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		log.info("Received a POST request to get all accounts.");
 		doGet(request, response);
 	}
 
