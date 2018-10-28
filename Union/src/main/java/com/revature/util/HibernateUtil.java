fdc9ecfa7bdbab199d491e35f1858555aa649aa0
Daniel Vosecky <dvosecky@protonmail.com>
2018-10-28 14:06:36 -0500

    non-deprecated hibernateUtil

--- Union/src/main/java/com/revature/util/HibernateUtil.java
+++ Union/src/main/java/com/revature/util/HibernateUtil.java
@@ -2,13 +2,20 @@ package com.revature.util;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 
-	private static SessionFactory sessionfactory = new Configuration().configure().buildSessionFactory();
+//	private static SessionFactory sessionfactory = new Configuration().configure().buildSessionFactory();
+	
 	
 	public static Session getSession() {
+		Configuration configuration = new Configuration().configure();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+				configuration.getProperties()).build();
+		SessionFactory sessionfactory = configuration.buildSessionFactory(serviceRegistry);
 		return sessionfactory.openSession();
 	}
 	
